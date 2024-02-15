f18e690558e7a41185d424e60fd8ae7e254e25a5
danula-ded <117920938+danula-ded@users.noreply.github.com>
2024-02-15 19:52:16 +0300

    Footer

--- app/components/Footer/Footer.jsx
+++ app/components/Footer/Footer.jsx
@@ -2,20 +2,20 @@ import Styles from './Footer.module.css'
 
 export const Footer = () => {
     return(
-        <footer class={Styles["footer"]}>
-          <a href="./index.html" class={Styles["footer__logo"]}>
-            <span class={Styles["footer__logo-name"]}>pindie</span
-            ><span class={Styles["footer__logo-copy"]}>, XXI век</span>
+        <footer className={Styles["footer"]}>
+          <a href="./index.html" className={Styles["footer__logo"]}>
+            <span className={Styles["footer__logo-name"]}>pindie</span
+            ><span className={Styles["footer__logo-copy"]}>, XXI век</span>
           </a>
-          <ul class={Styles["social-list"]}>
-            <li class={Styles["social-list__item"]}>
-              <a href="" class={`button ${Styles["social-list__link"]}`}>YT</a>
+          <ul className={Styles["social-list"]}>
+            <li className={Styles["social-list__item"]}>
+              <a href="" className={`button ${Styles["social-list__link"]}`}>YT</a>
             </li>
-            <li class={Styles["social-list__item"]}>
-              <a href="" class={`button ${Styles["social-list__link"]}`}>ВК</a>
+            <li className={Styles["social-list__item"]}>
+              <a href="" className={`button ${Styles["social-list__link"]}`}>ВК</a>
             </li>
-            <li class={Styles["social-list__item"]}>
-              <a href="" class={`button ${Styles["social-list__link"]}`}>TG</a>
+            <li className={Styles["social-list__item"]}>
+              <a href="" className={`button ${Styles["social-list__link"]}`}>TG</a>
             </li>
           </ul>
         </footer>
